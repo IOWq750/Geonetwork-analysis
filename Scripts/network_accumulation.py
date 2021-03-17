58a6e3b65895d86c57f40b2064bd125d1b649935
IOWq750 <karpach-am@yandex.ru>
2021-03-17 23:04:14 +0300

    Update network_accumulation.py

--- Scripts/network_accumulation.py
+++ Scripts/network_accumulation.py
@@ -9,7 +9,6 @@ G = nx.read_shp(r'{0}'.format(input_shp))
 nodes = G.nodes()
 edges = G.edges()
 total_dict = nx.get_edge_attributes(G, field)
-print(total_dict)
 for node in nodes:
     ancestors = list(nx.ancestors(G, node))
     sum_weight = 0
@@ -17,11 +16,14 @@ for node in nodes:
         for edge in edges:
             if edge[0] == coords:
                 sum_weight += float(total_dict[edge])  # sum weight for upslope edges
-                print(sum_weight)
+    source_list = []
+    [source_list.append(edge) if edge[0] == node else 'skip' for edge in edges]
+    num_sources = len(source_list)
+    arcpy.AddMessage(num_sources)
     for edge in edges:
         if edge[0] == node:
             self_weight = float(total_dict[edge])  # weight of the edge itself
-            accum_edge = {edge: sum_weight + self_weight}
+            accum_edge = {edge: sum_weight/num_sources + self_weight}
             arcpy.AddMessage(str(edge) + " has accumulated weight of " + str(sum_weight + self_weight))
             nx.set_edge_attributes(G, accum_edge, 'Accum')
 nx.write_shp(G, r'{0}'.format(output_workspace))
